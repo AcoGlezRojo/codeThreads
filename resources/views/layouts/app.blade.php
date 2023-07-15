d725109928ef99b8b8a53aad54d6bedaa15a7512
Acoidan <acoidan@pinchaaqui.es>
2023-07-15 22:51:47 +0100

    app layout and register form template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title') | CodeThreads</title>
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="bg-gray-100">
+
+    <header
+        class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <nav class="">
+            <div class="container flex flex-wrap items-center justify-between mx-auto py-4 px-4 md:px-0">
+                <!-- login -->
+                <a href="/" class="flex items-center">
+                    <img src="{{ asset('assets/logo/svgviewer-output.svg') }}" class="h-8 mr-3" alt="Logo">
+                </a>
+                <!-- buttons -->
+                <div class="flex md:order-2 gap-x-2">
+                    @guest
+                        <a
+                            class="
+                            md:block hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            {{ __('Login') }}
+                        </a>
+                        <a href="{{ url('register') }}"
+                            class="md:block hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            {{ __('Crear cuenta') }}
+                        </a>
+                    @endguest
+                    <button id="navbar-burger" data-collapse-toggle="navbar-menu" type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-menu" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+                <!-- menu -->
+                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-menu">
+                    <ul
+                        class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="{{ url('/') }}"
+                                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        </li>
+                    </ul>
+                    <div class="flex justify-center mt-3">
+                        @guest
+                            <a
+                                class="
+                            md:hidden w-1/2  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                {{ __('Login') }}
+                            </a>
+                            <a href="{{ url('register') }}"
+                                class="md:hidden w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                {{ __('Crear cuenta') }}
+                            </a>
+                        @endguest
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="mt-16">
+        @yield('body')
+    </main>
+
+    <footer>
+        <div class="text-center p-5 container mx-auto text-gray-500 font-bold">
+            © {{ now()->year }} CodeThreads - Todos los derechos reservados
+        </div>
+    </footer>
+
+    <script>
+        // Burger menus
+        document.addEventListener('DOMContentLoaded', function() {
+            // open
+            let burger = document.getElementById('navbar-burger');
+            let menu = document.getElementById('navbar-menu');
+
+            menu.classList.toggle('hidden');
+
+            burger.addEventListener('click', function() {
+                menu.classList.toggle('hidden');
+            });
+
+
+        });
+    </script>
+</body>
+
+</html>
